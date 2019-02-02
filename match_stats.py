68f45c09a2cf0a6802bee28d5e8c9198378ae719
Chinmay Joshi <chinmay0075@gmail.com>
2019-02-02 12:02:24 +0100

    Calculation of the performance of player in a game

--- match_stats.py
+++ match_stats.py
@@ -20,15 +20,24 @@ def calc_stats(team,players):
     for p in players:
         graph.run("MATCH (p:Player {name:'"+p+"'})-[q:PLAYS]-(g:Game {name:'"+sys.argv[1].split("_")[0]+" v/s "+sys.argv[1].split("_")[1]+"'}) SET g."+team+"_fouls = g."+team+"_fouls + q.fouls, g."+team+"_totalShots = g."+team+"_totalShots + q.shots, g."+team+"_yellowCards = g."+team+"_yellowCards + q.yellow_cards, g."+team+"_redCards = g."+team+"_redCards + q.red_cards")
 
-def findWinner(home_team,home_players,away_team,away_players):
+def findWinnerAndCalcPerf(home_team,home_players,away_team,away_players):
     home_sum = 0
     away_sum = 0
+    perf_home = {}
+    perf_away = {}
     for p in home_players:
-        for g in graph.run("MATCH (p:Player {name:'"+p+"'})-[q:PLAYS]-(g:Game {name:'"+sys.argv[1].split("_")[0]+" v/s "+sys.argv[1].split("_")[1]+"'}) return q.goals as goals"):
+        player_score = 0
+        for g in graph.run("MATCH (p:Player {name:'"+p+"'})-[q:PLAYS]-(g:Game {name:'"+sys.argv[1].split("_")[0]+" v/s "+sys.argv[1].split("_")[1]+"'}) return q.goals as goals, q.assists as assists, q.chances as chances, q.tackles as tackles, q.passes as passes, q.goals_saved as goals_saved"):
             home_sum = home_sum + g["goals"]
+            player_score = (1 * g["goals"]) + (0.75 * g["assists"]) + (0.65 * g["chances"]) + (0.5 * g["tackles"]) + (0.25 * g["passes"]) + (0.75 * g["goals_saved"])
+            perf_home[p] = player_score 
+
     for p in away_players:
-        for g in graph.run("MATCH (p:Player {name:'"+p+"'})-[q:PLAYS]-(g:Game {name:'"+sys.argv[1].split("_")[0]+" v/s "+sys.argv[1].split("_")[1]+"'}) return q.goals as goals"):
+        player_score = 0
+        for g in graph.run("MATCH (p:Player {name:'"+p+"'})-[q:PLAYS]-(g:Game {name:'"+sys.argv[1].split("_")[0]+" v/s "+sys.argv[1].split("_")[1]+"'}) return q.goals as goals, q.assists as assists, q.chances as chances, q.tackles as tackles, q.passes as passes, q.goals_saved as goals_saved"):
             away_sum = away_sum + g["goals"]
+            player_score = (1 * g["goals"]) + (0.75 * g["assists"]) + (0.65 * g["chances"]) + (0.5 * g["tackles"]) + (0.25 * g["passes"]) + (0.75 * g["goals_saved"])
+            perf_away[p] = player_score
     winner = ''
     if(home_sum > away_sum):
         winner = home_team
@@ -37,7 +46,7 @@ def findWinner(home_team,home_players,away_team,away_players):
     else:
         winner = away_team
     graph.run("MATCH (g:Game {name:'"+sys.argv[1].split("_")[0]+" v/s "+sys.argv[1].split("_")[1]+"'}) SET g.score = '"+home_team+" "+str(home_sum)+"  -  "+str(away_sum)+" "+away_team+"', g.winner = '"+winner+"'")
-
+    return perf_home,perf_away
 
 
 home_team = sys.argv[1].split("_")[0]
@@ -48,7 +57,13 @@ away_players = get_list_of_players(away_team)
 print("Away players are {}".format(away_players))
 calc_stats(home_team,home_players)
 calc_stats(away_team,away_players)
-findWinner(home_team,home_players,away_team,away_players)
+perf_home,perf_away = findWinnerAndCalcPerf(home_team,home_players,away_team,away_players)
+
+for a in perf_home:
+    print("The name of the player is {} and his performance is {}".format(a,perf_home[a]))
+
+for a in perf_away:
+    print("The name of the player is {} and his performance is {}".format(a,perf_away[a]))
 
 
 
