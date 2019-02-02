25c7305a26a7302f1b852ae14b64e14625eabe48
chinmay81192 <chinmay0075@gmail.com>
2019-02-02 16:37:05 +0100

    Mongo DB user stories
    user story 1-a : select playing 11 based on the formation and form.
    user story 1-b : propose top 5 forward players based on the budget of the club and the market value of players

---
+++ UserStory1-a.py.py
@@ -0,0 +1,62 @@
+import pymongo
+
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myClient["FootballDBMongo"]
+
+
+userInputTeam = input("--- Enter the Team name ---")
+
+print("--- Enter the formation ---")
+
+numOfDefenders = input("How many defenders do you need?")
+numOfAttacker = input("How many attackers do you need?")
+numOfMidfielder = input("How many midfielders do you need?")
+
+
+if numOfDefenders != 0:
+
+    queryString_partOne = {"$and": [{"Position": "Defender"}, {"Performance": {"$gt": "1"}}, {"Team": userInputTeam}]}
+    queryString_partTwo = {"Name": 1, "_id": 0, "Performance": 1, "Position": 1}
+    queryString_partThree ="{" " Performance" ":-1}"
+
+    x = mydb.playerStats.find(queryString_partOne, queryString_partTwo)
+    sortedList = x.sort("Performance", pymongo.DESCENDING)
+    listOfDocuments = sortedList.limit(int(numOfDefenders))
+
+    print("---LIST OF SUITABLE DEFENDERS---")
+    for eachDocument in sortedList:
+
+            print(eachDocument)
+
+
+if numOfAttacker != 0:
+
+    queryString_partOne = {"$and": [{"Position": "Attacker"}, {"Performance": {"$gt": "1"}}, {"Team": userInputTeam}]}
+    queryString_partTwo = {"Name": 1, "_id": 0, "Performance": 1,"Position": 1}
+    queryString_partThree ="{" " Performance" ":-1}"
+
+    x = mydb.playerStats.find(queryString_partOne, queryString_partTwo)
+    sortedList = x.sort("Performance", pymongo.DESCENDING)
+    listOfDocuments = sortedList.limit(int(numOfAttacker))
+
+    print("---LIST OF SUITABLE ATTACKERS---")
+    for eachDocument in sortedList:
+
+            print(eachDocument)
+
+
+if numOfMidfielder != 0:
+
+    queryString_partOne = {"$and": [{"Position": "Midfielder"}, {"Performance": {"$gt": "1"}}, {"Team": userInputTeam}]}
+    queryString_partTwo = {"Name": 1, "_id": 0, "Performance": 1, "Position": 1}
+    queryString_partThree ="{" " Performance" ":-1}"
+
+    x = mydb.playerStats.find(queryString_partOne, queryString_partTwo)
+    sortedList = x.sort("Performance", pymongo.DESCENDING)
+    listOfDocuments = sortedList.limit(int(numOfMidfielder))
+
+    print("--LIST OF SUITABLE MIDFIELDERS--")
+    for eachDocument in sortedList:
+
+            print(eachDocument)
+
---
+++ UserStory1-b.py.py
@@ -0,0 +1,34 @@
+import pymongo
+
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myClient["FootballDBMongo"]
+
+
+print("This query will give the top 5 attackers based on your team budget and player base price ")
+userInputTeam = input("Enter the team name : ")
+
+queryString_partOne = {}
+
+queryString_partOne = {"name": userInputTeam}
+queryString_partTwo = {"marketValue": 1, "_id": 0}
+
+x = mydb.clubStats.find(queryString_partOne, queryString_partTwo)
+
+
+for firstValue in x:
+
+    for a in firstValue:
+
+        queryString_partThree = {
+            "$and": [{"Position": "Attacker"}, {"BasePrice": {"$lt": firstValue[a]}}, {"Team": {"$ne": userInputTeam}}]}
+
+        queryString_partFour = {"Name": 1, "_id": 0, "BasePrice": 1, "Team": 1}
+
+    y = mydb.playerStats.find(queryString_partThree, queryString_partFour)
+    sortedList = y.sort("BasePrice", pymongo.DESCENDING)
+    listOfDocuments = sortedList.limit(5)
+
+    print("---LIST OF SUITABLE ATTACKERS IN YOUR PRICE RANGE---")
+    for eachDocument in sortedList:
+        print(eachDocument)
+
