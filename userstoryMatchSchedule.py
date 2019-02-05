3b9796e8b11491332984b7dd8abcae6119c62ff9
nivedithasooda <39679739+nivedithasooda@users.noreply.github.com>
2019-02-05 22:31:17 +0100

    Updated get match schedule

--- userstoryMatchSchedule.py
+++ userstoryMatchSchedule.py
@@ -1,13 +1,32 @@
 from py2neo import Graph, Node, Relationship
 import datetime
+import timedelta
 
 def neo4j():
     graphHost='localhost'
     graphUser = "neo4j"
-    graphPassphrase = "check"
+    graphPassphrase = "test"
     graph=Graph(bolt=True, host=graphHost, user=graphUser, password=graphPassphrase)
     return graph
 
+def getMatchScheduleBasedOnDate():
+    graph=neo4j()
+    print("Find the match schedule in a season:")
+    date=input("Enter the date(yyyy-mm-dd):")
+    format_str = "%Y-%m-%d" # The format
+    try:
+        fromDate = datetime.datetime.strptime(date, format_str)
+        fromDateStr=datetime.datetime.strftime(fromDate,format_str)
+        toDate=fromDate + datetime.timedelta(days=120)
+        toDateStr=datetime.datetime.strftime(toDate,format_str)
+        year=datetime.datetime.now().year-1
+        allSeasons=graph.run("match(n:Game {year:'"+str(year)+"'}) where n.schedule > date('"+fromDateStr+"') and n.schedule < date('"+toDateStr+"') return toString(n.schedule) as timing,n.name,n.location")
+        for game in allSeasons:
+                print("Game "+game["n.name"]+" on "+game["timing"]+" at "+game["n.location"])
+    except:
+        print("Invalid date format")
+
+           
 def getMatchSchedule():
     graph=neo4j()
     print("Find the match schedule in a season:")
@@ -17,4 +36,5 @@ def getMatchSchedule():
     for game in allSeasons:
         print(game["n.name"]+" to take place in "+game["n.schedule"])
         
-getMatchSchedule()
+getMatchScheduleBasedOnDate()
+#getMatchSchedule()
