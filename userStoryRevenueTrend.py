f8be4a9dcc9319454305919d59eea30ce741d136
Chinmay Joshi <chinmay0075@gmail.com>
2019-02-05 12:01:15 +0100

    Revenue generation user story

--- userStoryRevenueTrend.py
+++ userStoryRevenueTrend.py
@@ -4,7 +4,17 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["FootballDBMongo"]
 mycol = mydb["clubStats"]
 
-club = input("Please enter the name of the club whose revenue trend you want to see")
+def calcAnnualRevenueGrowth(revenue,year):
+    start = revenue[0]
+    end = revenue[len(revenue)-1]
+    yearRange = int(abs(year[0]-year[len(year)-1]))
+
+    annualGrowth = (pow(end/start,1/yearRange) - 1) * 100
+    print("The annual revenue growth for the club is {}%".format(annualGrowth))
+
+
+
+club = input("Please enter the name of the club whose revenue trend you want to see ")
 year = []
 revenue = []
 clubData = mycol.find_one({"name":club})
@@ -16,10 +26,16 @@ for x in clubData:
         for y in clubData[x]:
             year.append(int(y["Year"]))
             revenue.append(int(y["Revenue"]))
+
+calcAnnualRevenueGrowth(revenue,year)
 plt.plot(year,revenue)
+plt.xlabel('Year')
+plt.ylabel('Revenue in millions of Euros')
 plt.show()
 
 
 
-
+#References : 
+#Calculation of annual revenue growth % : https://www.fool.com/knowledge-center/how-to-calculate-revenue-growth-for-3-years.aspx
+#Revenues of football clubs in million euros : https://en.wikipedia.org/wiki/Deloitte_Football_Money_League#2018
 
