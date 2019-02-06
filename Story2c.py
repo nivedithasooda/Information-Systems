1684a5d99349b13ec28e1fe676a1822d34935cd8
SelesAbraham <30854461+SelesAbraham@users.noreply.github.com>
2019-02-06 02:14:13 +0100

    Latest Story 2c changes

--- Story2c.py
+++ Story2c.py
@@ -16,28 +16,29 @@ if not databaseCollection:
     print("              No data")
     
 else:
-    
+    x=[]
     for titles in databaseCollection:
+        
         print("      Year in which the club won EPL Championship")
         print("           Year                Title")
         for w in databaseCollection[titles]:
             if(w["TrophyName"] == "EPL"):
-    
+                x.append(w["year"])
                 print("           {}                {}".format(w["year"],w["TrophyName"]))
-                break
+            
         print()
-        x = w["year"]
+        print("      Points earned in the season ")
+        print("           Year                Points Earned")
         #Ponts earned in that season
         for w in databaseCollection[titles]:
-            if(w["year"] == x ):
-                        seasonQuery1 = {"$and":[{"name": userInputClub},{"SeasonPoints.Points":{"$gt": "18"}},{"SeasonPoints.Year":{"$eq":x}}]}
+            if(w["year"] in x ):
+                        seasonQuery1 = {"$and":[{"name": userInputClub},{"SeasonPoints.Points":{"$gt": "18"}}]}
                         seasonQuery2 = {"SeasonPoints":1 ,"_id" :0}
                         databaseCollection1 = mydb.clubStats.find_one(seasonQuery1,seasonQuery2)
                         
                         for season in databaseCollection1:
-                            print("      Points earned in the season ")
-                            print("           Year                Points Earned")
+                           
                             for w1 in databaseCollection1[season]:
                                 if(w1["Year"] == w["year"]):
                                     print("           {}                {}".format(w1["Year"],w1["Points"]))
-                            print()
+                           
